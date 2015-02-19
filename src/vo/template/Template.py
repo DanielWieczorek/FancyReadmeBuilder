bf33fe05f8453e1a4c4a1d20f24c638dc71228f8
Daniel Wieczore <daniel.wieczorek0@googlemail.com>
2015-02-19 20:03:55 +0100

    fixed failed test

--- src/vo/template/Template.py
+++ src/vo/template/Template.py
@@ -18,7 +18,10 @@ class Template(object):
         """
         self._image = Canvas(image)
         self.field_list = list()
-        self.field_list.extend(fields)
+        if isinstance(fields,list):
+            self.field_list.extend(fields)
+        else:
+            self.field_list.append(fields)
 
     def __iter__(self):
         return iter(self.field_list)
